326ff9391e236f659400a8ab0099ba7e8e59e069
Your Name <94886034+saumyaakumarasinghe@users.noreply.github.com>
2022-12-01 10:20:38 +0530

    NavBar components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import Logo from '../assets/iceCreamLogo.jpg'                   //Import logo picture 
+import { Link } from 'react-router-dom'
 
 
 function NavBar() {
   return (
-    <div className='navbar'>
+    <div className='navbar'> 
+
         <div className='leftSide'>
             <img src={Logo}/> 
         </div>
-        <div className='rightSide'></div>
+
+        <div className='rightSide'>
+          <Link to='/'> Home </Link>
+          <Link to='/menu'> Menu </Link>
+          <Link to='/about'> About </Link>
+          <Link to='/contact'> Contact </Link>
+        </div>
+        
     </div>
   )
 }
